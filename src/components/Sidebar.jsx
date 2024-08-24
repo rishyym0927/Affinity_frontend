cb0001f63a11796e5f75ddaad025d7237c3a7257
rishyym0927 <rishyym0927@gmail.com>
2024-08-24 18:43:09 +0530

    Added profile page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import profile_svg from "../assets/profile_svg.svg";
 
 const Sidebar = () => {
   const { user, logoutUser } = useContext(AuthContext);
   const location = useLocation();
+  const [showProfileBox, setShowProfileBox] = useState(false);
 
-  // Animation variants for menu items
   const menuItemVariants = {
     hover: {
       scale: 1.05,
-      backgroundColor: "#4B5563", // Tailwind's gray-600
+      backgroundColor: "#4B5563",
       transition: {
         type: "spring",
         stiffness: 300,
       },
-      cursor : "none"
+      cursor: "none",
     },
+  };
+
+  const profileBoxVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: { type: "spring", stiffness: 200, damping: 20 },
+    },
+    exit: { opacity: 0, scale: 0.8, transition: { duration: 0.3 } },
+  };
+
+  const overlayVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 0.3 } },
+    exit: { opacity: 0, transition: { duration: 0.3 } },
   };
 
   const getLinkClass = (path) => {
@@ -29,7 +45,8 @@ const Sidebar = () => {
   };
 
   const getListItemClass = (path) => {
-    const baseClass = "mb-6 w-full p-3 rounded-lg text-lg md:text-xl lg:text-2xl font-bold ";
+    const baseClass =
+      "mb-6 w-full p-3 rounded-lg text-lg md:text-xl lg:text-2xl font-bold ";
     return location.pathname === path
       ? `${baseClass} bg-[#2C2C2C]`
       : `${baseClass} bg-neutral-700`;
@@ -37,20 +54,112 @@ const Sidebar = () => {
 
   return (
     <div className="w-full md:w-80 lg:w-96 h-full bg-black text-white flex flex-col fixed top-0 left-0">
-      {/* Profile section remains unchanged */}
       <motion.div
-        className="p-3 flex gap-2 flex-row items-center bg-[#ff0059] hover:bg-red-500 m-5 rounded-lg justify-around  "
+        className="p-3 flex gap-2 flex-row items-center bg-[#ff0059] hover:bg-red-500 m-5 rounded-lg justify-around"
         whileHover={{ scale: 1.05 }}
+        onClick={() => setShowProfileBox(true)}
       >
         <img
           src={user?.image_url || profile_svg}
           alt="Profile"
-          className="w-14 h-14 rounded-full bg-black "
+          className="w-14 h-14 rounded-full bg-black"
         />
         <h3 className="text-3xl font-semibold">
           @{user?.user_name || "John Doe"}
         </h3>
       </motion.div>
+
+      <AnimatePresence>
+        {showProfileBox && (
+          <>
+            <motion.div
+              className="fixed inset-0 bg-black bg-opacity-60 backdrop-blur-sm z-10"
+              variants={overlayVariants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              onClick={() => setShowProfileBox(false)}
+            />
+            <motion.div
+              className="fixed top-[5%] left-[10%] right-[10%] max-h-[90vh] overflow-y-auto p-4 bg-neutral-800 rounded-xl z-20 shadow-lg text-white"
+              variants={profileBoxVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <div className="flex flex-col md:flex-row w-full gap-4">
+                <div className="w-full md:w-1/3 flex justify-center items-start">
+                  <img
+                    src={user?.image_url || profile_svg}
+                    alt="Profile"
+                    className="w-full max-w-[260px] h-auto rounded-lg object-cover"
+                  />
+                </div>
+                <div className="w-full md:w-2/3 space-y-4 text-neutral-300">
+                  <h2 className="text-4xl font-extrabold text-[#ff0059] mb-4">
+                    {user?.first_name} {user?.last_name}
+                  </h2>
+                  <div className="grid grid-cols-2 gap-4">
+                    <InfoItem
+                      label="Username"
+                      value={user?.user_name || "John Doe"}
+                    />
+                    <InfoItem
+                      label="Email"
+                      value={user?.email || "john.doe@example.com"}
+                    />
+                    <InfoItem
+                      label="Gender"
+                      value={user?.gender || "Not specified"}
+                    />
+                    <InfoItem
+                      label="Age"
+                      value={user?.age || "Not specified"}
+                    />
+                    <InfoItem
+                      label="Location"
+                      value={user?.location || "Not specified"}
+                    />
+                    <InfoItem
+                      label="Personality"
+                      value={user?.openness || "Not specified"}
+                    />
+                    <InfoItem
+                      label="Interests"
+                      value={user?.interest || "Not specified"}
+                    />
+                    <InfoItem
+                      label="Looking for"
+                      value={user?.relation_type || "Not specified"}
+                    />
+                    <InfoItem
+                      label="Social habits"
+                      value={user?.social_habits || "Not specified"}
+                    />
+                    <InfoItem
+                      label="Expectations"
+                      value={user?.exp_qual || "Not specified"}
+                    />
+                    <InfoItem
+                      label="Past relationships"
+                      value={user?.past_relations || "Not specified"}
+                    />
+                  </div>
+                </div>
+              </div>
+              <motion.button
+                className="mt-4 bg-[#ff0059] w-full text-white font-semibold px-4 py-2 rounded-lg shadow-lg transform transition-all duration-300 text-lg"
+                whileHover={{ scale: 1.05, backgroundColor: "#ff3380" }}
+                whileTap={{ scale: 0.95 }}
+                onClick={() => setShowProfileBox(false)}
+              >
+                Close
+              </motion.button>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
+
       <nav className="flex-1 p-6 w-full cursor-none">
         <ul className="w-full cursor-none">
           {user.gender === "Female" && (
@@ -94,7 +203,7 @@ const Sidebar = () => {
             </Link>
           </motion.li>
           <motion.li
-            className={getListItemClass("/chatbot")}
+            className={getListItemClass("/queue")}
             variants={menuItemVariants}
             whileHover="hover"
           >
@@ -104,17 +213,31 @@ const Sidebar = () => {
           </motion.li>
         </ul>
       </nav>
-      {/* Logout button remains unchanged */}
-      <motion.div
-        className="w-[28%] flex items-center justify-center m-6 bg-neutral-700 p-3 rounded-lg text-xl font-bold cursor-pointer"
-        variants={menuItemVariants}
-        whileHover="hover"
-        onClick={logoutUser}
+
+      <motion.button
+        className="mt-4 bg-[#ff0059] w-2/6 ml-4 mb-4 text-white font-semibold px-4 py-2 rounded-lg shadow-lg transform transition-all duration-300 text-lg"
+        whileHover={{ scale: 1.1, backgroundColor: "#ff3380" }}
+        whileTap={{ scale: 0.95 }}
+        onClick={logoutUser }
+        animate={{
+          y: [0, -10, 0], // Moves the button up and down
+        }}
+        transition={{
+          repeat: Infinity, // Keeps the animation looping
+          duration: 0.4, // Duration of each animation cycle
+          ease: "easeInOut", // Smooth animation
+        }}
       >
-        Logout
-      </motion.div>
+        Close
+      </motion.button>
     </div>
   );
 };
+
+const InfoItem = ({ label, value }) => (
+  <p className="text-lg">
+    <span className="font-semibold text-yellow-500">{label}:</span> {value}
+  </p>
+);
 
 export default Sidebar;
