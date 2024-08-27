6ffde0a2beae41d3c73faf1007db848d5d45eefa
rishyym0927 <rishyym0927@gmail.com>
2024-08-27 11:21:36 +0530

    Fixed

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,10 +1,10 @@
 export const AI_CHATBOT_URL="http://ec2-3-6-94-25.ap-south-1.compute.amazonaws.com:9000/chat"
 export const MACHINE_CHATBOT_URL="http://ec2-3-6-94-25.ap-south-1.compute.amazonaws.com:9000/match"
 export const AI_MAIN_URL="http://ec2-3-6-94-25.ap-south-1.compute.amazonaws.com:9000/"
-export const RUST_MAIN_URL ="http://ec2-3-7-69-234.ap-south-1.compute.amazonaws.com:3001/"
-export const RUST_BACKEND_URL_SCORE="http://ec2-3-7-69-234.ap-south-1.compute.amazonaws.com:3001/updatescore"
+export const RUST_MAIN_URL ="http://ec2-13-232-216-30.ap-south-1.compute.amazonaws.com:3001/"
+export const RUST_BACKEND_URL_SCORE="http://ec2-13-232-216-30.ap-south-1.compute.amazonaws.com:3001/updatescore"
 
-export const chatBaseURL = "http://ec2-13-233-131-217.ap-south-1.compute.amazonaws.com:1243/api";
+export const chatBaseURL = "http://ec2-3-111-34-89.ap-south-1.compute.amazonaws.com:1243/api";
 import axios from "axios";
 
 export const postRequest = async (url, body) => {
